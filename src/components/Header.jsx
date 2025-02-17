23a8f1c836a30d03691e0a2d8533bb42ef5b948a
Axel Hurt <axel12hurt@gmail.com>
2025-02-17 16:42:59 -0300

    Filtro de busqueda en header, intercambio de datos entre componentes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,30 +1,8 @@
 import { Link } from "react-router-dom";
 
-function Header() {
+function Header({ setSearch }) {
+
   return (
-    // <header class="bg-[#cf1015] text-white h-25 items-center justify-center  sticky top-0 z-50 grid grid-cols-3">
-    //   <div className="col-span-2">
-    //     <h1 class="text-4xl font-semibold text-center uppercase text-[#e7e7e7]">
-    //       Fake Store
-    //     </h1>
-    //   </div>
-
-    //   <div className="flex">
-    //     <input type="text" placeholder="Buscar" />
-    //     <button className="text-2xl m-1.5">
-    //       <i class="bi bi-search"></i>
-    //     </button>
-
-    //     <button className="text-2xl m-1.5">
-    //       <i class="bi bi-basket2-fill"></i>
-    //     </button>
-
-    //     <button className="text-2xl m-1.5">
-    //       <i class="bi bi-person-fill"></i>
-    //     </button>
-    //   </div>
-    // </header>
-
     <header className="bg-[#cf1015] text-white h-25 sticky top-0 z-50 grid grid-cols-3 items-center px-5">
       <div></div>
 
@@ -39,6 +17,7 @@ function Header() {
           type="text"
           placeholder="Buscar"
           className="bg-[#f9e0e2] text-[#eb2d24] h-12 w-90 p-5 border-1 rounded-md"
+          onChange={(e) => setSearch(e.target.value)}
         />
         <button className="text-2xl">
           <i className="bi bi-search"></i>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,13 +3,29 @@ import Header from '../components/Header'
 import Enlaces from '../components/Enlaces'
 import Products from '../components/Products'
 import Footer from '../components/Footer'
+import { useState, useEffect } from "react";
 
 function HomePage() {
+  const URL_TIENDA = "https://fakestoreapi.com/products";
+
+  // FILTROS DE BUSQUEDA
+  const [data, setData] = useState([]); // Datos de la API
+  const [search, setSearch] = useState(""); // Estado del filtro
+
+  useEffect(() => {
+    fetch(URL_TIENDA)
+      .then((response) => response.json())
+      .then((data) => {
+        setData(data);
+      })
+      .catch((error) => console.error("Error al cargar los datos:", error));
+  }, []);
+
   return (
     <>
-        <Header />
+        <Header setSearch={setSearch} />
         <Enlaces />
-        <Products />
+        <Products data={data} search={search} />
         <Footer />
     </>
   )
